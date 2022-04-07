7456da9226a6116c104e665ebfc6911153e2a838
Arturio <78929526+Arthur200133396@users.noreply.github.com>
2022-04-07 22:31:16 +0500

    Add files via upload

---
+++ Circle_Bresenham/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void PutPixel(Graphics g, Color col, int x, int y, int alpha)
+        {
+            g.FillRectangle(new SolidBrush(Color.FromArgb(alpha, col)), x, y, 1, 1);
+        }
+        public static void BresenhamCircle(Graphics g, Color clr, int _x, int _y, int radius)
+        {
+            int x = 0, y = radius, gap = 0, delta = (2 - 2 * radius);
+            while (y >= 0)
+            {
+                PutPixel(g, clr, _x + x, _y + y, 255);
+                PutPixel(g, clr, _x + x, _y - y, 255);
+                PutPixel(g, clr, _x - x, _y - y, 255);
+                PutPixel(g, clr, _x - x, _y + y, 255);
+                gap = 2 * (delta + y) - 1;
+                if (delta < 0 && gap <= 0)
+                {
+                    x++;
+                    delta += 2 * x + 1;
+                    continue;
+                }
+                if (delta > 0 && gap > 0)
+                {
+                    y--;
+                    delta -= 2 * y + 1;
+                    continue;
+                }
+                x++;
+                delta += 2 * (x - y);
+                y--;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            BresenhamCircle(g, Color.Black, 470, 250, 200);
+        }
+    }
+}
